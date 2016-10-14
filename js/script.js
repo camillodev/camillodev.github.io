e60ede645d3a7165a7465ad5b03e484e46175069
Camillo Dev <camillodeveloper@gmail.com>
2016-10-14 00:49:56 -0300

    Slide de projetos semi pronto

--- js/script.js
+++ js/script.js
@@ -1,9 +1,48 @@
-$(document).ready(function () {
-    $("#owl-demo").owlCarousel({
-        autoPlay: 4000
-        , items: 4
-        , itemsDesktop: [1199, 3]
-        , itemsDesktopSmall: [979, 3]
+$(function(){
+    var width = (parseInt($(".carrossel .item").outerWidth() + + parseInt($(".carrossel .item").css("margi-right")))) * $(".carrosel .item").length;    
+    $(".carrossel").css("width", width);
+    
+    var numImages = 1;
+    var marginPadding = 30;
+    
+    ident = 0;
+    var count = ($(".carrossel .item").length / numImages) - 1;
+    var slide  = (numImages + marginPadding) + ($(".carrossel .item").outerWidth() + numImages);
+    
+    $(".forth").click(function(){
+       if (ident < count) {
+           ident++;
+           $(".carrossel").animate({"margin-left": "-=" + slide + "px"}, 500);
+       }
     });
 
 });
+
+
+
+function automatizar(segundos) {
+  var back = null;
+
+  var auto = setInterval(function () {
+  if ($('.carrossel').css('margin-left') === '0px') {
+   ident = '0';
+  }
+
+  if (ident < count && back === null) {
+   ident++;
+   $('.carrossel').animate({'margin-left': '-=' + slide + 'px'}, 500);
+  } else {
+   if (ident === 1) {
+    back = null;
+   } else {
+    back = true;
+   }
+   ident--;
+   $('.carrossel').animate({'margin-left': '+=' + slide + 'px'}, 500);
+  }
+
+  $('.forth, .back').click(function () {
+   clearInterval(auto);
+  });
+ }, segundos * 1000);
+}
